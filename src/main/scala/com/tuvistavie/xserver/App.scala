2428762a9e74d61cdceef5679b45929c4b16ef61
tuvistavie <tuvistavie@gmail.com>
2013-05-24 02:00:28 +0900

    Update logging configuration to use slf4j with log4j backend.

--- src/main/scala/com/tuvistavie/xserver/App.scala
+++ src/main/scala/com/tuvistavie/xserver/App.scala
@@ -3,11 +3,8 @@ package com.tuvistavie.xserver
 import io.Server
 import util.Properties.{ settings => Config }
 
-import com.typesafe.scalalogging.slf4j.Logging
-
-object App extends Logging {
+object App {
   def main(args: Array[String]) = {
-    logger.debug("foo")
     val displayNumber = Config.getInt("server.default-display")
     Server.startUp(displayNumber)
   }
