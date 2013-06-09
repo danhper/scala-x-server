ddfe431e0d10e0e192c02aa0602188834c383e4d
tuvistavie <tuvistavie@gmail.com>
2013-06-10 01:04:07 +0900

    Add login page and some basic logic.

--- frontend/app/controllers/Application.scala
+++ frontend/app/controllers/Application.scala
@@ -3,11 +3,21 @@ package com.tuvistavie.xserver.frontend.controllers
 import play.api._
 import play.api.mvc._
 
+import com.tuvistavie.xserver.frontend.forms._
 import com.tuvistavie.xserver.frontend.auth.UnixAuthentication
 
 object Application extends Controller with UnixAuthentication {
 
   def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+  def doLogin() = Action { implicit request =>
+    val (user, password) = loginForm.bindFromRequest.get
     Ok(views.html.index("Your new application is ready."))
   }
 
---
+++ frontend/app/forms/LoginForm.scala
@@ -0,0 +1,15 @@
+package com.tuvistavie.xserver.frontend
+
+
+import play.api.data._
+import play.api.data.Forms._
+
+
+package object forms {
+  val loginForm = Form(
+    tuple(
+      "username" -> text,
+      "password" -> text
+    )
+  )
+}
