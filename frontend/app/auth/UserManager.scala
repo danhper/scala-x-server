63119467e6070fc6477407cf7da1c6b96ac86203
tuvistavie <tuvistavie@gmail.com>
2013-06-13 01:59:56 +0900

    Fix UserManager to change token generation depending on environment.

--- frontend/app/auth/UserManager.scala
+++ frontend/app/auth/UserManager.scala
@@ -1,7 +1,8 @@
 package com.tuvistavie.xserver.frontend.auth
 
-import play.api.Play
+import play.api.{ Play, Mode }
 
+import scala.util.Random
 import java.security.MessageDigest
 import sun.misc.BASE64Encoder
 
@@ -9,13 +10,23 @@ case class User(id: Int, name: String, token: String)
 
 object UserManager {
   val current = new UserManager {
-    val hasher = new Sha512Hasher
-  }
+    import Play.current
 
+    val hasher = Play.mode match {
+      case Mode.Prod => new Sha512Hasher
+      case _ => new NoOpHasher
+    }
+
+    val tokenGenerator = Play.mode match {
+      case Mode.Prod => new RandomTokenGenerator
+      case _ => new SimpleTokenGenerator
+    }
+  }
 }
 
 trait UserManager {
   protected val hasher: Hasher
+  protected val tokenGenerator: TokenGenerator
 
   private val maxUsers = Play.current.configuration.getInt("application.max_connections").get
   private var users = Map[String, User]()
@@ -27,22 +38,48 @@ trait UserManager {
   def createUser(username: String, password: String): User = {
     val id = availableIds.indexOf(true)
     availableIds(id) = false
-    val token = hasher.hash(username + password)
+    val token = hasher.hash(tokenGenerator.generateToken(username, password))
     val user = User(id, username, token)
     users += (token -> user)
     user
   }
 }
 
+trait TokenGenerator {
+  def generateToken(username: String, password: String): String
+}
+
+class SimpleTokenGenerator extends TokenGenerator {
+  override def generateToken(username: String, password: String): String = {
+    username + password
+  }
+}
+
+class RandomTokenGenerator extends TokenGenerator {
+  override def generateToken(username: String, password: String): String = {
+    val randomString = Random.nextString(Play.current.configuration.getInt("auth.random-string-length").get)
+    val timeStamp = (System.currentTimeMillis.toString)
+    username + randomString + timeStamp + password
+  }
+}
+
 trait Hasher {
+  def hash(str: String): String
+}
+
+class NoOpHasher extends Hasher {
+  override def hash(str: String) = str
+}
+
+trait DigestHasher extends Hasher {
   val hashAlgorithm: String
-  def hash(str: String): String = {
+  override def hash(str: String): String = {
     val md = MessageDigest.getInstance(hashAlgorithm)
     val encoder = new BASE64Encoder()
     encoder.encode(md.digest(str.getBytes))
   }
 }
 
-class Sha512Hasher extends Hasher {
+class Sha512Hasher extends DigestHasher {
   override val hashAlgorithm = "SHA-512"
 }
