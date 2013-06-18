1b735c37ecb40b9234d0422a32fe2b322e7b3d6c
tuvistavie <tuvistavie@gmail.com>
2013-06-18 11:28:16 +0900

    Add other errors.

--- bridge/src/main/scala/com/tuvistavie/xserver/protocol/errors.scala
+++ bridge/src/main/scala/com/tuvistavie/xserver/protocol/errors.scala
@@ -24,7 +24,25 @@ object XError extends Logging {
       _ = iterator.skip(21)
     } yield {
       code match {
-        case 1 => error.Request(sequenceNumber, minorOpcode, majorOpcode)
+        case  1 => error.Request(sequenceNumber, minorOpcode, majorOpcode)
+        case  2 => error.Value(sequenceNumber, data, minorOpcode, majorOpcode)
+        case  3 => error.Window(sequenceNumber, data, minorOpcode, majorOpcode)
+        case  4 => error.Pixmap(sequenceNumber, data, minorOpcode, majorOpcode)
+        case  5 => error.Atom(sequenceNumber, data, minorOpcode, majorOpcode)
+        case  6 => error.Cursor(sequenceNumber, data, minorOpcode, majorOpcode)
+        case  7 => error.Font(sequenceNumber, data, minorOpcode, majorOpcode)
+        case  8 => error.Match(sequenceNumber, minorOpcode, majorOpcode)
+        case  9 => error.Drawable(sequenceNumber, data, minorOpcode, majorOpcode)
+        case 10 => error.Access(sequenceNumber, minorOpcode, majorOpcode)
+        case 11 => error.Alloc(sequenceNumber, minorOpcode, majorOpcode)
+        case 11 => error.Alloc(sequenceNumber, minorOpcode, majorOpcode)
+        case 12 => error.Colormap(sequenceNumber, data, minorOpcode, majorOpcode)
+        case 13 => error.GContext(sequenceNumber, data, minorOpcode, majorOpcode)
+        case 14 => error.IDChoice(sequenceNumber, data, minorOpcode, majorOpcode)
+        case 15 => error.Name(sequenceNumber, minorOpcode, majorOpcode)
+        case 16 => error.Length(sequenceNumber, minorOpcode, majorOpcode)
+        case 17 => error.Implementation(sequenceNumber, minorOpcode, majorOpcode)
+        case _ => error.BadError
       }
     }
   }
@@ -55,9 +73,117 @@ abstract class XError (
 }
 
 package error {
+  case object BadError extends XError(0, 0, 0, 0, 0)
+
   case class Request (
-    override val sequenceNumber: Int,
-    override val minorOpcode: Int,
-    override val majorOpcode: Int
-  ) extends XError(1, sequenceNumber, 0, minorOpcode, majorOpcode)
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(1, seqNum, 0, minOpCode, majOpCode)
+
+  case class Value (
+    seqNum: Int,
+    badValue: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(2, seqNum, badValue, minOpCode, majOpCode)
+
+  case class Window (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(3, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class Pixmap (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(4, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class Atom (
+    seqNum: Int,
+    badAtomId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(5, seqNum, badAtomId, minOpCode, majOpCode)
+
+  case class Cursor (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(6, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class Font (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(7, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class Match (
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(8, seqNum, 0, minOpCode, majOpCode)
+
+  case class Drawable (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(9, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class Access (
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(10, seqNum, 0, minOpCode, majOpCode)
+
+  case class Alloc (
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(11, seqNum, 0, minOpCode, majOpCode)
+
+  case class Colormap (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(12, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class GContext (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(13, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class IDChoice (
+    seqNum: Int,
+    badResourceId: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(14, seqNum, badResourceId, minOpCode, majOpCode)
+
+  case class Name (
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(15, seqNum, 0, minOpCode, majOpCode)
+
+  case class Length (
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(16, seqNum, 0, minOpCode, majOpCode)
+
+  case class Implementation (
+    seqNum: Int,
+    minOpCode: Int,
+    majOpCode: Int
+  ) extends XError(17, seqNum, 0, minOpCode, majOpCode)
 }
