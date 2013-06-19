4022b8f84bac2d6b321f947e8fe9fa6961cff27d
tuvistavie <tuvistavie@gmail.com>
2013-06-19 10:14:43 +0900

    Fix startup script to be able to stop program as normal user.

--- auth_server/src/main/scala/com/tuvistavie/authmanager/AuthServer.scala
+++ auth_server/src/main/scala/com/tuvistavie/authmanager/AuthServer.scala
@@ -1,21 +1,39 @@
 package com.tuvistavie.authmanager
 
-import akka.actor.Actor
+import akka.actor.{ Actor, ActorSystem, Props }
+import scala.sys.process.Process
+import com.typesafe.config.ConfigFactory
 
 case class Authenticate (
   username: String,
   password: String,
-  block: (_ => Unit)
+  block: (() => Unit)
 )
+
+case object Success
+case object Failure
 
 class AuthManager extends Actor {
   def receive = {
-    case Authenticate(u, password, block) => {
-
+    case Authenticate(username, password, block) => {
+      val authenticated = checkAuthentication(username, password)
+      if(authenticated) {
+        block()
+        sender ! Success
+      } else sender ! Failure
     }
   }
 
-  private def checkAuthentication: Boolean = {
-    false
+  private def checkAuthentication(username: String, password: String): Boolean = {
+    val pb = Process(AuthManager.authPath, Seq(username, password))
+    val exitCode: Int = pb.!
+    exitCode == 0
   }
+}
+
+object AuthManager {
+  val config = ConfigFactory.load()
+  val system = ActorSystem("AuthServer")
+  val ref = system.actorOf(Props[AuthManager], "authManager")
+  val authPath = config.getString("paths.nix-password-checker")
 }
