2310ad9d7254247d827d140d195ee4efd1d0e1b0
tuvistavie <tuvistavie@gmail.com>
2013-06-26 19:04:26 +0900

    Add configuration for JS.

--- frontend/public/javascripts/app.js
+++ frontend/public/javascripts/app.js
@@ -1,10 +1,12 @@
 define([
-  'Logger'
-], function(Logger) {
+  'Logger',
+  'config'
+], function(Logger, config) {
 
   var initialize = function() {
     Logger.useDefaults();
-    Logger.debug("initializing application");
+    Logger.debug("initializing application with config:");
+    Logger.debug(config);
   };
 
   return {
