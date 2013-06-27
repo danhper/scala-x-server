1a4fd50f45834e6cf89f3e6f40f0552e29364630
tuvistavie <tuvistavie@gmail.com>
2013-06-28 06:18:41 +0900

    Add classes for 'test'.

--- frontend/public/javascripts/app.js
+++ frontend/public/javascripts/app.js
@@ -1,12 +1,25 @@
 define([
   'Logger',
-  'config'
-], function(Logger, config) {
+  'config',
+  'controllers/WsWrapper'
+], function(Logger, config, WS) {
 
   var initialize = function() {
     Logger.useDefaults();
     Logger.debug("initializing application with config:");
     Logger.debug(config);
+    initializeWebsocket();
+  };
+
+  var initializeWebsocket = function() {
+    var socket = new WS();
+    socket.addOnOpenListener('connect', function() {
+      console.log('connected');
+    });
+    socket.addOnMessageListener('echo', function(message) {
+      console.log(message.data);
+    });
+    socket.connect(config.wsURL);
   };
 
   return {
