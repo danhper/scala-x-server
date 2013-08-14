57c071d217232240e8c9862e0be72ae6fca46fc5
tuvistavie <tuvistavie@gmail.com>
2013-08-14 12:09:20 +0900

    Add launch backend to AuthServer.

--- auth_server/src/main/scala/com/tuvistavie/authmanager/AuthServer.scala
+++ auth_server/src/main/scala/com/tuvistavie/authmanager/AuthServer.scala
@@ -5,6 +5,7 @@ import scala.sys.process.Process
 import com.typesafe.config.ConfigFactory
 
 case class Authenticate (
+  id: Int,
   username: String,
   password: String
 )
@@ -13,11 +14,15 @@ case object Success
 case object Failure
 
 class AuthManager extends Actor with ActorLogging {
+  private val clientBasePort = AuthManager.config.getInt("xbridge-server.client.base-port")
+  private val binPath = AuthManager.config.getString("paths.backend")
+
   def receive = {
-    case Authenticate(username, password) => {
+    case Authenticate(id, username, password) => {
       log.debug("trying to authenticate {}", username)
       val authenticated = checkAuthentication(username, password)
       if(authenticated) {
+        launchBackend(id, username)
         sender ! Success
       } else sender ! Failure
     }
@@ -29,6 +34,15 @@ class AuthManager extends Actor with ActorLogging {
     val exitCode: Int = pb.!
     log.debug("auth exitcode: {}", exitCode)
     exitCode == 0
+  }
+
+  private def launchBackend(id: Int, username: String) {
+    log.debug("starting backend app")
+    val port = s"wrapper.java.additional.2=-Dbridge.akka.remote.netty.port=${clientBasePort + id}"
+    // FIXME: use data received from browser instead of hard coded dimensions
+    val args = List("start", port, "--", "-n", id.toString, "-w", 800.toString, "-h", 600.toString)
+    val process = Process(binPath :: args, None, "RUN_AS_USER" -> username)
+    process.run()
   }
 }
 
